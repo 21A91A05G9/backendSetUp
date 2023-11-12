64f104a8447a5a7c4bc391fc13773e8eb8f7c417
21A91A05G9 <21a91a05g9@aec.edu.in>
2023-11-12 10:51:46 +0530

    backend files

---
+++ backend/app.js
@@ -0,0 +1,11 @@
+import express  from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import cors from "cors";
+const app=express();
+app.use(bodyParser.json())
+app.use(cors())
+mongoose.connect('mongodb+srv://vasavi_08:ptWwCJboGyUQIXFu@cluster0.yhpakpu.mongodb.net/DriveReady?retryWrites=true&w=majority')
+.then(()=> app.listen(5011))
+.then(()=>console.log("Connected to Database & Listening to localhost 5011"))
+.catch((err)=>console.log(err));
