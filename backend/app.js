a43f166beddb8bdb8f2ff4d94146407e70471ee3
VASAVI <97425195+21A91A05G9@users.noreply.github.com>
2023-11-16 19:33:26 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ import cors from "cors";
 const app=express();
 app.use(bodyParser.json())
 app.use(cors())
-mongoose.connect('mongodb+srv://vasavi_08:ptWwCJboGyUQIXFu@cluster0.yhpakpu.mongodb.net/DriveReady?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://vasavi_08:<password>@cluster0.yhpakpu.mongodb.net/DriveReady?retryWrites=true&w=majority')
 .then(()=> app.listen(5011))
 .then(()=>console.log("Connected to Database & Listening to localhost 5011"))
 .catch((err)=>console.log(err));
